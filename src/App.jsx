0caea756cb7049d1f0eb2ecb272e507a06058532
cleitonrs <rscleitonrs@gmail.com>
2024-02-19 15:11:30 -0300

    parallax effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import "./app.scss"
+import "./app.scss";
 import { Hero } from "./components/hero/Hero";
+import { Parallax } from "./components/parallax/Parallax";
 import { Sidebar } from "./components/sidebar/Sidebar";
 
 const App = () => {
-  return <div>
-    <section id="Homepage">
-      <Sidebar />
-      <Hero />
-    </section>
-    <section id="Services">Parallax</section>
-    <section>Services</section>
-    <section id="Portfolio">Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id="Contact">Contact</section>
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Sidebar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" /> Parallax
+      </section>
+      <section>Services</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+        Parallax
+      </section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
