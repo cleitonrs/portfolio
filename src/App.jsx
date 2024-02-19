f25b00a9e053fdb61106932929793be30f3e7958
cleitonrs <rscleitonrs@gmail.com>
2024-02-19 07:01:14 -0300

    animated sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./app.scss"
+import { Sidebar } from "./components/sidebar/Sidebar";
 
 const App = () => {
   return <div>
-    <section> <a href="">Hello</a></section>
-    <section>Parallax</section>
+    <section id="Homepage">
+      <Sidebar />
+    </section>
+    <section id="Services">Parallax</section>
     <section>Services</section>
-    <section>Parallax</section>
+    <section id="Portfolio">Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section>
+    <section id="Contact">Contact</section>
   </div>;
 };
 
